57bb829e246ade2ba0026118780eb03a5ddc6c17
Gary <gary.yan2@gmail.com>
2013-10-30 17:11:35 -0700

    update

--- simple1D.m
+++ simple1D.m
@@ -141,22 +141,51 @@ errorerror=max(abs(E-e))
 %%%
 clear all
 close all
-N = 80;
-s = 0.00001;%.001%1/N;
-h0 = -s/(1-(1+s)^N);
-h = zeros(N+2,1);
-x = zeros(N+2,1);
+N = 160;
+% s = .0002%1/N;
+% h0 = -s/(1-(1+s)^N);
+% h = zeros(N+2,1);
+% x = zeros(N+2,1);
+% 
+% for i = 1:N+2
+%     h(i) = h0*(1+s)^(i-2);
+%     if i == 1
+%         x(i) = -h(1)/2;
+%     elseif (i == 2)
+%         x(i) = h0/2;
+%     else
+%         x(i) = x(i-1)+(h(i-1)+h(i))/2;
+%     end
+% end
 
-for i = 1:N+2
-    h(i) = h0*(1+s)^(i-2);
-    if i == 1
-        x(i) = -h(1)/2;
-    elseif (i == 2)
-        x(i) = h0/2;
-    else
-        x(i) = x(i-1)+(h(i-1)+h(i))/2;
-    end
+rng(1234);
+h0 = 1/N;
+X = zeros(N+1,1);
+for i = 1:N+1
+   X(i) = (i-1)*h0; 
+   if(i>1 && i < N+1)
+   X(i) = X(i) + (-1+rand*(2))*h0/3;
+   end
 end
+x = zeros(N+2,1);
+for i = 2:N+1
+    x(i) = (X(i-1)+X(i))/2;
+end
+x(1) = -x(2);
+x(N+2) = 1+(1-x(N+1));
+%plot(X,1,'*',x,1,'o')
+%xlim([0 1])
+h = zeros(N+2,1);
+for i = 2:N+1
+   h(i) = X(i)-X(i-1); 
+end
+
+h(1) = h(2);
+h(N+2) = h(N+1);
+
+
+
+
 f = zeros(N+2,1);
 for i = 1:N+2
     if i==1
