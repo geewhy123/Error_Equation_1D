f33ba7299bda49e714afe4fa24f47e65e347262d
Gary Yan <yankai@chronos.local>
2013-12-02 09:58:24 -0800

    1129

--- errordriver1D.m
+++ errordriver1D.m
@@ -1,11 +1,11 @@
 close all
 clear all
 rng(1234);
-load('test20.mat')
-u = nu20;
+load('hN20.mat')
+u = hu20;
 N = 20;
 h0 = 1/N;
-k=0.0005;
+k=0.005;
 X = zeros(N+1,1);
 for i = 1:N+1
    X(i) = (i-1)*h0; 
@@ -46,7 +46,7 @@ end
 
 
 [R,uxx,Z] =computeres(u,x,h,N,f);
-max(abs(R))
+r=max(abs(R))
 
 
 
@@ -56,53 +56,53 @@ e = zeros(N+2,1);
 ee =zeros(N+2,1);
 
 % 
-tic
-olderr = 1;
-%esterr= zeros(,
-T = 1;
-for j = 1:100000
-
-    
-    %error based on u.....
-[errerr(j),Z]=unstructuredrecon3(e,x,h,N,0,0);
-%if(j > 50 && ((abs(errerr(j)-olderr)/olderr < 1e-15) || abs(errerr(j)-olderr)<1e-15))
-if(j > 50 && k*s <1e-15)
-    T = (1:1:j)*k;
-    break
-end
-s=0;
-olderr = errerr(j);
-for i= 2:N+1
-    y = Z(:,i);
-    yr = Z(:,i+1);
-    yl = Z(:,i-1);
-    %need averaging flux
-upr1 = y(2)+2*y(3)*h(i)/2+3*y(4)*(h(i)/2)^2;
-upr2 = yr(2)+2*yr(3)*-h(i+1)/2+3*yr(4)*(-h(i+1)/2)^2;
-upr = (upr1+upr2)/2;
-upl1 = y(2)+2*y(3)*-h(i)/2+3*y(4)*(-h(i)/2)^2;
-upl2 = yl(2)+2*yl(3)*h(i-1)/2+3*yl(4)*(h(i-1)/2)^2;
-upl = (upl1+upl2)/2;
-
-if i==2
-    upl = upl1;
-end
-if i==N+1
-   upr = upr1; 
-end
-
-ee(i) = e(i) + k*((upr-upl)/h(i)+R(i)); 
-s=max(s,abs((upr-upl)/h(i)+R(i)));
-end
-
-e = ee;
-
-T = (1:1:j)*k;
-
-
-s
-end
-toc
+% tic
+% olderr = 1;
+% %esterr= zeros(,
+% T = 1;
+% for j = 1:100000
+% 
+%     
+%     %error based on u.....
+% [errerr(j),Z]=unstructuredrecon3(e,x,h,N,0,0);
+% %if(j > 50 && ((abs(errerr(j)-olderr)/olderr < 1e-15) || abs(errerr(j)-olderr)<1e-15))
+% if(j > 50 && k*s <1e-15)
+%     T = (1:1:j)*k;
+%     break
+% end
+% s=0;
+% olderr = errerr(j);
+% for i= 2:N+1
+%     y = Z(:,i);
+%     yr = Z(:,i+1);
+%     yl = Z(:,i-1);
+%     %need averaging flux
+% upr1 = y(2)+2*y(3)*h(i)/2+3*y(4)*(h(i)/2)^2;
+% upr2 = yr(2)+2*yr(3)*-h(i+1)/2+3*yr(4)*(-h(i+1)/2)^2;
+% upr = (upr1+upr2)/2;
+% upl1 = y(2)+2*y(3)*-h(i)/2+3*y(4)*(-h(i)/2)^2;
+% upl2 = yl(2)+2*yl(3)*h(i-1)/2+3*yl(4)*(h(i-1)/2)^2;
+% upl = (upl1+upl2)/2;
+% 
+% if i==2
+%     upl = upl1;
+% end
+% if i==N+1
+%    upr = upr1; 
+% end
+% 
+% ee(i) = e(i) + k*((upr-upl)/h(i)+R(i)); 
+% s=max(s,abs((upr-upl)/h(i)+R(i)));
+% end
+% 
+% e = ee;
+% 
+% T = (1:1:j)*k;
+% 
+% 
+% s
+% end
+% toc
 
 
 
