f1d82a932cfb7ba5ad70a728deff4b2125da7130
Gary <gary.yan2@gmail.com>
2014-04-06 17:55:05 -0700

    new

---
+++ lowerorder/linearrecon/Periodic/update.m
@@ -0,0 +1,29 @@
+function [uu,d] = update(eqn,u,x,f,k,h,N,p,t,phys,uder,j,time,gsp,Rsp)
+
+%UPDATESOLN Summary of this function goes here
+%   Detailed explanation goes here
+
+
+    
+   switch t
+       case 1
+      [uu,d] = rk1(u,x,f,k,h,N,p,t,phys);
+
+       case 2
+           [uu,d] = rk2(u,x,f,k,h,N,p,t,phys);
+           
+       case 4
+           [uu,d] = rk4(u,x,f,k,h,N,p,t,phys);
+           
+       case 5
+           [uu,d] = rk5(u,x,f,k,h,N,p,t,phys);
+           
+       case 7
+           [uu,d] = rk7(eqn,u,x,f,k,h,N,p,t,phys,uder,j,time,gsp,Rsp);
+       otherwise
+   end
+      
+
+
+end
+
