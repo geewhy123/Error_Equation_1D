3d0214e0fc45c5a75fdb442d1e8d1a8269cc9861
Gary Yan <yankai@chronos.local>
2014-04-08 11:05:59 -0700

    before better Ut

--- lowerorder/linearrecon/Periodic/recon/linear/recon1.m
+++ lowerorder/linearrecon/Periodic/recon/linear/recon1.m
@@ -12,7 +12,7 @@ wi4 = 1;%/abs(x4-xi)^w;
 
 
 
-xbi = (1/hi)*(((xi+hi/2)-xi)^2/2-((xi-hi/2)-xi)^2/2);
+% xbi = (1/hi)*(((xi+hi/2)-xi)^2/2-((xi-hi/2)-xi)^2/2);
 
 ub1 = u1;
 ub2 = u2;
@@ -24,10 +24,10 @@ b = [wi1*(ub1-ubi); wi2*(ub2-ubi); wi3*(ub3-ubi); wi4*(ub4-ubi) ];
 
 global AD
 
-y(2:2) = AD(:,:,i)*b;
+y(2) = AD(:,:,i)*b;
 
 
-y(1) = ubi-xbi*y(2);%ubi-xbi*y(2)
+y(1) = ubi;%-xbi*y(2);%ubi-xbi*y(2)
 %q = y(1)-ubi
 
 
